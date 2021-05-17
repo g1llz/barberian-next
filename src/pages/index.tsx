50f548be734a3d0916d773b13292e14c289aac1a
Gilles Lima <gillestoim@gmail.com>
2021-05-16 21:19:04 -0300

    feat: add tailwind css

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Barberian</title>
+        <meta name="description" content="A barber app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.tsx'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled']
+    }
+  },
+  plugins: []
+}
